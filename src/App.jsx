27873e448286c2841fbda04e5092d719cd18c503
Clemencia Ibanez Padilla <clemenciaibanezpadilla@gmail.com>
2023-08-09 22:33:12 -0300

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,37 @@
 
-import './index.css'
-import './App.css'
-import { Carrousel } from './Components/Carrousel/Carrousel'
-import { Header } from "./Components/Header/Header"
-import ItemListContainer from "./Components/ItemListContainer/ItemListContainer"
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
-import ItemDetailContainer from "./Components/ItemDetailContainer/ItemDetailContainer";
-import PortadaCat from './Components/PortadaCat/PortadaCat'
-import { Categories } from './Components/Categories/Categories'
-import { Footer } from "./Components/Footer/Footer"
-import { Contact } from "./Components/Contact/Contact"
-import { Store } from './Components/Store/Store'
-import { About } from './Components/About/About'
-import { Delivery } from './Components/Delivery/Delivery'
-import { Guarantee } from './Components/Guarantee/Guarantee'
-import { Why } from './Components/WhyPlantopia/WhyPlantopia'
-import { PageNotFound } from './Components/PageNotFound/PageNotFound'
-import CategoriesMenu from './Components/CategoriesMenu/CategoriesMenu'
-import { CartProvider } from "./context/CartContext";
+import React, { useContext } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { DarkModeProvider } from "./context/DarkModeContext";
+import { CartProvider } from "./context/CartContext";
+import { WishProvider } from "./context/WishContext";
+import { SearchProvider } from "./context/SearchContext";
+import { AuthContextProvider } from "./context/AuthContext";
+import { Header } from "./Components/Header/Header";
+import { Carrousel } from "./Components/Carrousel/Carrousel";
+import { CategoriesMenu } from "./Components/CategoriesMenu/CategoriesMenu";
+import { Categories } from "./Components/Categories/Categories";
+import ItemListContainer from "./Components/ItemListContainer/ItemListContainer";
+import PortadaCat from "./Components/PortadaCat/PortadaCat";
+import ItemDetailContainer from "./Components/ItemDetailContainer/ItemDetailContainer";
+import Delivery from "./Components/Delivery/Delivery";
+import Guarantee from "./Components/Guarantee/Guarantee";
+import PageNotFound from "./Components/PageNotFound/PageNotFound";
+import Store from "./Components/Store/Store";
+import Contact from "./Components/Contact/Contact";
+import About from "./Components/About/About";
+import Why from "./Components/WhyPlantopia/WhyPlantopia";
+import Footer from "./Components/Footer/Footer";
 import WishView from "./Components/WishView/WishView";
 import CartView from "./Components/CartView/CartView";
-import { SearchProvider } from './context/SearchContext'
 import Checkout from "./Components/Checkout/Checkout";
-import { ProductNotFound } from './Components/ProductNotFound/ProductNotFound'
-import { WishProvider } from './context/WishContext'
-import { AuthContextProvider } from "./context/AuthContext"
-import LoginScreen from "./Components/Auth/LoginScreen"
-import { useContext } from "react"
-import { AuthContext } from "./context/AuthContext"
-import RegisterScreen from "./Components/Auth/RegisterScreen"
-
+import ProductNotFound from "./Components/ProductNotFound/ProductNotFound";
+import LoginScreen from "./Components/Auth/LoginScreen";
+import RegisterScreen from "./Components/Auth/RegisterScreen";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
+  const { user } = useContext(AuthContext);
 
-  const { user } = useContext(AuthContext)
-  
   return (
     <div>
       <AuthContextProvider>
@@ -43,83 +39,92 @@ function App() {
           <CartProvider>
             <WishProvider>
               <BrowserRouter>
-              {
-              user.logged
-              ? <>
-                <SearchProvider>
-                  <Header />        
-                  
+                {user.logged ? (
+                  <>
+                    <SearchProvider>
+                      <Header />
+
+                      <Routes>
+                        <Route
+                          path="/"
+                          element={
+                            <div>
+                              <Carrousel />
+                              <CategoriesMenu />
+                              <Categories />
+                              <ItemListContainer />
+                              <Delivery />
+                              <Guarantee />
+                            </div>
+                          }
+                        />
+
+                        <Route
+                          path="/products/:categoryId"
+                          element={
+                            <div>
+                              <PortadaCat />
+                              <CategoriesMenu />
+                              <Categories />
+                              <ItemListContainer />
+                              <Delivery />
+                              <Guarantee />
+                            </div>
+                          }
+                        />
+
+                        <Route
+                          path="/detail/:itemId"
+                          element={<ItemDetailContainer />}
+                        />
+
+                        <Route path="/wish" element={<WishView />} />
+
+                        <Route path="/cart" element={<CartView />} />
+
+                        <Route path="/checkout" element={<Checkout />} />
+
+                        <Route
+                          path="/contact"
+                          element={
+                            <div>
+                              <Store />
+                              <Contact />
+                            </div>
+                          }
+                        />
+                    <Route
+                          path="/about"
+                          element={
+                            <div>
+                              <About />
+                              <Why />
+                            </div>
+                          }
+                        />
+
+                        <Route path="*" element={<PageNotFound />} />
+                      </Routes>
+                      <Footer />
+                    </SearchProvider>
+                  </>
+                ) : (
                   <Routes>
-                    <Route path="/" element={ 
-                        <div>
-                          <Carrousel/>
-                          <CategoriesMenu/>
-                          <Categories/>
-                          <ItemListContainer />
-                          <Delivery/>
-                          <Guarantee/>
-                        </div> }/>
-                    
-                    <Route path="/products/:categoryId" element={
-                        <div>
-                          <PortadaCat/>
-                          <CategoriesMenu/>
-                          <Categories/>
-                          <ItemListContainer />
-                          <Delivery/>
-                          <Guarantee/>              
-                        </div>}/>
-
-                    
-                    <Route path="/detail/:itemId" element={ <ItemDetailContainer /> }/>
-
-                    <Route path="/wish" element={ <WishView /> }/>
-
-                    <Route path="/cart" element={ <CartView /> }/>
-                    
-                    <Route path="/checkout" element={ <Checkout/> }/>
-                    
-
-                  
-                    <Route path="/contact" element={ 
-                        <div>
-                          <Store/>
-                          <Contact />                                
-                        </div>
-                    }/>
-                    <Route path="/about" element={ 
-                        <div>
-                          <About/>
-                          <Why/>
-                          
-                        </div>
-                    }/>          
-                    <Route path="/*" element={ <PageNotFound/> }/>          
+                    <Route path="/login" element={<LoginScreen />} />
+                    <Route path="/register" element={<RegisterScreen />} />
+                    <Route path="*" element={<Navigate to="/login" />} />
                   </Routes>
-                  <Footer/>  
-                </SearchProvider>
-                </>
-
-              : <Routes>
-                  <Route path="/login" element={<LoginScreen />}/>
-                  <Route path="/register" element={<RegisterScreen />}/>
-                  <Route path="*" element={<Navigate to="/login"/>}/>
-                </Routes>
-            }    
+                )}
               </BrowserRouter>
             </WishProvider>
           </CartProvider>
         </DarkModeProvider>
       </AuthContextProvider>
-
-
-            
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
 
    
             
